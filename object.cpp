b696d2ad77a9d977ea160353a464ccdfc173b01a
s256226 <167795105+s256226@users.noreply.github.com>
2025-05-20 13:16:59 +0900

    Update object.cpp

--- object.cpp
+++ object.cpp
@@ -1,45 +1,48 @@
-﻿#include "gg.h"
+#include "gg.h"
 using namespace gg;
 
 #include "object.h"
 
 //
-// 頂点配列オブジェクトの作成
+// 頂点配列オブジェクトの作成（モーフィング対応）
 //
 //   vertices: 頂点数
-//   position: 頂点の二次元位置 (GLfloat[3] の配列)
+//   p0: 変形前の頂点位置 GLfloat[3]
+//   p1: 変形後の頂点位置 GLfloat[3]
 //   lines: 線分数
-//   index: 線分の頂点インデックス
-//   戻り値: 作成された頂点配列オブジェクト名
+//   index: 線分インデックス
+//   戻り値: 作成された VAO 名
 //
-GLuint createObject(GLuint vertices, const GLfloat(*position)[3], GLuint lines, const GLuint* index)
+GLuint createObject(GLuint vertices, const GLfloat(*p0)[3], const GLfloat(*p1)[3], GLuint lines, const GLuint* index)
 {
-  // 頂点配列オブジェクト
-  GLuint vao;
+  GLuint vao, vbo[2], ibo;
+
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  // 頂点バッファオブジェクト
-  GLuint vbo;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, position, GL_STATIC_DRAW);
+  // p0 用 VBO
+  glGenBuffers(2, vbo);
 
-  // インデックスバッファオブジェクト
-  GLuint ibo;
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, p0, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+  glEnableVertexAttribArray(0); // layout(location = 0) in vec4 p0;
+
+  // p1 用 VBO
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, p1, GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+  glEnableVertexAttribArray(1); // layout(location = 1) in vec4 p1;
+
+  // インデックスバッファ
   glGenBuffers(1, &ibo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * lines, index, GL_STATIC_DRAW);
 
-  // 結合されている頂点バッファオブジェクトを in 変数から参照できるようにする
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(0);
-
-  // 頂点配列オブジェクトの結合を解除した後に頂点バッファオブジェクトとインデックスバッファオブジェクトの結合を解除する
+  // 後片付け
   glBindVertexArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 
   return vao;
 }
