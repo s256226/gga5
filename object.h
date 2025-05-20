c65e462fc0f58d0de4d3357c1d314418bb46807e
s256226 <167795105+s256226@users.noreply.github.com>
2025-05-20 13:17:16 +0900

    Update object.h

--- object.h
+++ object.h
@@ -1,10 +1,11 @@
-﻿//
-// 頂点配列オブジェクトの作成
+//
+// 頂点配列オブジェクトの作成（モーフィング対応バージョン）
 //
 //   vertices: 頂点数
-//   position: 頂点の二次元位置 (GLfloat[3] の配列)
+//   p0: 変形前の頂点位置
+//   p1: 変形後の頂点位置
 //   lines: 線分数
 //   index: 線分の頂点インデックス
-//   戻り値: 作成された頂点配列オブジェクト名
+//   戻り値: 作成された VAO
 //
-extern GLuint createObject(GLuint vertices, const GLfloat(*position)[3], GLuint lines, const GLuint* index);
+extern GLuint createObject(GLuint vertices, const GLfloat(*p0)[3], const GLfloat(*p1)[3], GLuint lines, const GLuint* index);
